74146599bcac5cd01647233f00883d4cb915e52f
fabian15944 <fabiandelacruzm@gmail.com>
2020-05-29 08:13:32 -0500

    Proyecto con cambios de camara

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  foto: any;
+
+  constructor(private camera: Camera) { }
+
+  hacerFoto() {
+    const options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      this.foto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
